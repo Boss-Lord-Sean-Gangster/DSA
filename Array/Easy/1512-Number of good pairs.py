279151a571e3b49f5a40c247fac339ea2911ff57
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-07-31 15:21:21 +0530

    first commit

---
+++ Array/Easy/1512-Number of good pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        length = len(nums)
+        result = 0
+        for i in range(length):
+            for j in range (i+1,length):
+                if nums[i]==nums[j]:
+                    result= result+1
+        return result            
+                    
