b77b306a62b7c6511aed8e2220ba3e352211f83c
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-08-17 16:40:18 +0530

    Solved Medium Level ROTATE ARRAY  Question(189)

--- Array/Medium/SortColors.cpp
+++ Array/Medium/SortColors.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int red=0;
-        int white=1;
-        int size = nums.size();
-        for(int index=0;index<size;index++){
-            if(nums[index]==0){
-               swap(nums[index],nums[red]);
-               red++;
-            }
-            if(nums[index]==1){
-                swap(nums[index],nums[white]);
-                white++;
-            }
+        int n = nums.size();
+        int blue=n-1;
+        int index =0 ;
+        while(index<=blue){
+           if(nums[index]==0){
+            swap(nums[index],nums[red]);
+            red++;
+            index++;
+           }
+           else if(nums[index]==2){
+            swap(nums[index],nums[blue]);
+            blue--;
+           }
+           else{
+            index++;
+           }
         }
+        
+        
 
     }
 };
