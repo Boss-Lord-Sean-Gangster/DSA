92c74c2f5e7cc405f264a14d2b8e3298a17b93f4
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-12-17 15:19:28 +0530

    Solved Array Easy Level Question CONTAINS DUPLICATE

--- Array/Easy/ContainsDuplicate.cpp
+++ Array/Easy/ContainsDuplicate.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-       bool result = false;
-       int n = nums.size();
-       for (int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(nums[i]==nums[j]){
-                result = true;
-            }
-        }
-       } 
-       return result;
+       unordered_set<int> set(nums.begin(),nums.end());
+       return set.size()<nums.size();
     }
 };
