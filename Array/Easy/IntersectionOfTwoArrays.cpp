7421b3567c5feb3c95c47071a18eaff3d187e94f
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-12-21 23:44:57 +0530

    Solved Easy level array question INTERSECTION OF TWO ARRAYS

---
+++ Array/Easy/IntersectionOfTwoArrays.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        int count[1002]={0};
+
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i =0;
+        int j = 0;
+
+        while((i<nums1.size()) && j<(nums2.size()))
+        {
+            if((nums1[i] == nums2[j]) && !count[nums1[i]])
+            {
+                ans.push_back(nums1[i]);
+                count[nums1[i]]++;
+                i++,j++;
+            }
+            else if(nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        return ans;
+
+    }
+};
