3d9173378bcb4e0f38186c4707dd6bce9c979f6b
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2025-01-22 22:06:08 +0530

    Solved an easy level linked-list question REMOVE LINKED LIST ELEMENTS

---
+++ LinkedList/Easy/RemoveLinkedListElements.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+     while(head!=nullptr && head->val==val){
+        ListNode* temp = head ;
+        head = temp->next;
+        temp->next=nullptr;
+        delete temp;
+     }
+     ListNode* prev = nullptr;
+     ListNode* curr = head;
+     while(curr!=nullptr){
+        if(curr->val==val){
+            prev->next=curr->next;
+            delete curr;
+            curr = prev->next;
+        }
+        else{
+            prev=curr;
+            curr=curr->next;
+        }
+     }
+     return head;
+    }
+};
